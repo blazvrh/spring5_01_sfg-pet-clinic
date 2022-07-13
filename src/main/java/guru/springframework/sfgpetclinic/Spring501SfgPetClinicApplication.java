cba615c1093bdec5d7ea8ca88c686efaddf02987
KrHo <krhodev@gmail.com>
2022-07-13 21:05:10 +0200

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfgpetclinic/Spring501SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring501SfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring501SfgPetClinicApplication.class, args);
+	}
+
+}
