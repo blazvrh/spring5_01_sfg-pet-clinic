08ccf27d44b1d2e6418e420eebe6973ebc756a48
KrHo <krhodev@gmail.com>
2022-09-04 17:42:07 +0200

    package rename

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/Spring501SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/Spring501SfgPetClinicApplicationTests.java
@@ -1,12 +1,13 @@
-package sfgpetclinic;
+package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Spring501SfgPetClinicApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
